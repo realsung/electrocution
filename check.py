622d9fb25c7aaec60c6cc42c67207c7dbaf9d90a
나봄하랑 <21sunrin052@sunrint.hs.kr>
2022-11-24 22:33:23 +0900

    search and copy asar files

---
+++ check.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+def createFolder(dir: str) -> None:
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    except OSError:
+        print(f"Error : Create directory. {dir}")
+
+def main():
+    path = "C:\\Users\\nabom\\AppData\\Local\\"
+
+    createFolder('./asar')
+    for (root, dirs, files) in os.walk(path):
+        for file in files:
+            if os.path.splitext(file)[1].lower() == ".asar":
+                if "Programs" in root:
+                    app = (root.split('Local')[-1].split("\\")[2])
+                else:
+                    app = (root.split('Local')[-1].split("\\")[1])
+                
+                print(f'"{app}" asar file : {file}')
+
+                shutil.copyfile(f'{root}\\{file}', f'./asar/{app}_{file}')
+
+if __name__ == '__main__':
+    main()
